598e9a6d04fc0cb2e4f175ca04983933f83dc7c6
itohemon <itohemon@gmail.com>
2020-04-29 06:27:48 +0900

    ROS2の単一ターゲット内での複数nodeの実行 https://qiita.com/NeK/items/aadb89da577e3706e674

---
+++ minimal_executor/src/multi_main.cpp
@@ -0,0 +1,19 @@
+#include <rclcpp/rclcpp.hpp>
+#include <minimal_comp/minimal_comp_node1.hpp>
+#include <minimal_comp/minimal_comp_node2.hpp>
+
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::executors::MultiThreadedExecutor exec;
+
+    auto node1 = std::make_shared<minimal_comp::MinimalCompNode1>();
+    exec.add_node(node1);
+    auto node2 = std::make_shared<minimal_comp::MinimalCompNode2>();
+    exec.add_node(node2);
+
+    exec.spin();
+    rclcpp::shutdown();
+
+    return 0;
+}
+
